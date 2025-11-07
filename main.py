1659172b5bb3f4709b93e16c17a74dc352e9ae2f
Kopaka <felixbruell@live.de>
2025-11-07 12:25:13 +0100

    added reflect and shear variant which is urrently same as ray lenght but with matrices

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ draw_guess = True
 iterations = 1
 iteration_strategies = ["Virtual iterations", "Real iterations"]
 iteration_strategy = 0  # index into iteration_strategies
-predict_strategies = ["ray diff", "ray length"]
+predict_strategies = ["ray diff", "ray length", "reflect and shear"]
 predict_strategy = 0  # index into predict_strategies
 useSpeed = False
 
@@ -146,6 +146,8 @@ def draw_scene():
         methodRayDiff(C0, dir, hits)
     if predict_strategy == 1:
         methodRayLength(C0, C1, dir, hits)
+    if predict_strategy == 2:
+        methodReflectAndShear(C0, dir, hits)
     
 
     ax.legend(loc="upper right")
@@ -213,6 +215,19 @@ def methodRayDiff(C0, dir, hits):
     else:
         draw_prediction(C0, newDir, hits)
 
+# mirrors scene at plane with 2D location O and 2D normal N (return 3x3 matrix)
+def matrixMirror(O, N):
+    nx, ny = N
+    ox, oy = O
+    d = nx * ox + ny * oy
+    M = np.array([
+        [1 - 2*nx*nx,   -2*nx*ny,    2*nx*d],
+        [-2*nx*ny,      1 - 2*ny*ny, 2*ny*d],
+        [0,             0,           1]
+    ])
+    return M
+
+
 def methodRayLength(C0, C1, dir, hits):
     rayLength = 0.0
     speed = 1.0
@@ -228,6 +243,28 @@ def methodRayLength(C0, C1, dir, hits):
         speed *= cosalpha / cosbeta
 
     newDir = C1 + dir * rayLength - C0
+    draw_prediction(C0, newDir, hits)
+
+# matrix multiplication
+def mul(A, B):
+    return np.matmul(A, B)
+
+def methodReflectAndShear(C0, dir, hits):
+    # initialize viewTransform with a 3x3 identity matrix
+    viewTransform = np.identity(3)
+    ray = Ray(C1, dir) # only used for tracking direction
+    for hit in hits[:-1]:
+        I = -ray.D() # incomming direction
+        ray = ray.transfer(hit).sampleNext(hit)
+        R = ray.D() # outgoing direction
+        H = (I + R) / np.linalg.norm(I + R)  # half-vector
+        viewTransform = mul(viewTransform, matrixMirror(hit.P(), H))
+
+    P = hits[-1].P()
+    Pnew = mul(viewTransform, np.array([P[0], P[1], 1.0]))[:2]
+
+    newDir = Pnew - C0
+    newDir /= np.linalg.norm(newDir)
     draw_prediction(C0, newDir, hits)
 
 def doRealIterations(C0, newDir, hits):
