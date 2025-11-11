7380655baa5b1f7d24652bf2eadec0bccea09607
kopaka <felixbruell@live.de>
2025-11-11 15:05:42 +0100

    more debugging for iterations

--- main.py
+++ main.py
@@ -60,12 +60,21 @@ refraction_scene = [
 ]
 addBox(refraction_scene, [-10, -10], [10, 10])
 
-refraction_scene2 = []
-addBox(refraction_scene2, [-10, -10], [10, 10])
+refraction_scene2 = [
+    Plane([10, -10], [10, 20]), # wall on the right
+    Plane([-10, -10], [10, -10]), # wall on the bottom
+]
+#addBox(refraction_scene2, [-10, -10], [10, 10])
 addCircle(refraction_scene2, [0, 0], [2, 2], faceOutside=True, ior=1.5, numSegments=32)
 addCircle(refraction_scene2, [0, 0], [1.5, 1.5], faceOutside=False, ior=1.5, numSegments=32)
 
-planes = refraction_scene2
+glass_globe_scene = [
+    Plane([10, -10], [10, 20]), # wall on the right
+    Plane([-10, -10], [10, -10]), # wall on the bottom
+]
+addCircle(glass_globe_scene, [0, 3], [3, 3], faceOutside=True, ior=1.5, numSegments=32)
+
+planes = glass_globe_scene
 
 def closestIntersect(ray: Ray, prevPlane: Plane | None = None) -> Hit | None:
     closest_hit = None
@@ -95,7 +104,7 @@ C0 = np.array([-10, 2.15])
 C1 = np.array([-8.55, 4.5])
 C1_angle = -46.8  # in degrees
 max_bounces = 3
-draw_differentials = False
+draw_differentials = True
 draw_guess = True
 draw_normals = False
 iterations = 1
@@ -105,12 +114,14 @@ predict_strategies = ["ray diff", "ray length", "reflect and shear"]
 predict_strategy = 0  # index into predict_strategies
 useSpeed = False
 useShear = True
+draw_last_iteration = True
 
 # labels
 LABEL_RAY = "original"
 LABEL_RAY2 = "guess"
 LABEL_RAY2_DIFF = "differential"
 LABEL_RAY2_PRED = "predicted"
+LABEL_RAY2_ITERATION = "last iteration"
 
 # ---------------------------------------------------------------
 # Draw functions
@@ -197,12 +208,12 @@ def methodRayDiff(C0, dir, hits):
         ray2 = ray2.transfer(hit2)
         curP = ray2.P() + ray2.dP()
         # draw ray differential segment
-        if draw_differentials:
+        if draw_differentials and iterations == 0:
             ax.plot([prevP[0], curP[0]], [prevP[1], curP[1]], 'c--', label=LABEL_RAY2_DIFF if hit == hits[0] else None)
 
         ray2 = ray2.sampleNext(hit2)
 
-        if draw_differentials:
+        if draw_differentials and iterations == 0:
             # debug D + dD
             ax.arrow(ray2.P()[0] + ray2.dP()[0], ray2.P()[1] + ray2.dP()[1], ray2.D()[0] + ray2.dD()[0], ray2.D()[1] + ray2.dD()[1], head_width=0.2, color='orange', length_includes_head=True)
 
@@ -217,7 +228,7 @@ def methodRayDiff(C0, dir, hits):
     if draw_guess:
         ax.plot(ray2.P()[0], ray2.P()[1], 'bo')
         ax.text(ray2.P()[0]+0.2, ray2.P()[1]+0.2, "P*", color='b')
-        if draw_differentials:
+        if draw_differentials and iterations == 0:
             dpend = ray2.P() + ray2.dP()
             ax.arrow(ray2.P()[0], ray2.P()[1], ray2.dP()[0], ray2.dP()[1], head_width=0.4, color='c', length_includes_head=True)
             ax.plot([ray2.P()[0], dpend[0]], [ray2.P()[1], dpend[1]], 'c-', lw=2.0)
@@ -319,11 +330,13 @@ def methodReflectAndShear(C0, dir, hits):
     draw_prediction(C0, newDir, hits)
 
 def doRealIterations(C0, dir, newDir, hits):
+    if len(hits) == 0:
+        return newDir
     P = hits[-1].P()
     # normal of the final intersection plane at P
     N = -dir
     if len(hits) >= 2:
-        dir = hits[-2].P() - hits[-1].P()
+        N = hits[-2].P() - hits[-1].P()
     
     PPlane = Plane(P, P + [-N[1], N[0]])  # create plane orthogonal to dir at P
 
@@ -339,8 +352,10 @@ def doRealIterations(C0, dir, newDir, hits):
         ray2 = Ray(C0, bestDir)
         stepMultiplier = 1.0
         if fails > 0:
-            exponent = ((-1) ** (fails)) * ((fails + 1) // 2)
-            stepMultiplier = pow(1.2, exponent)
+            #exponent = ((-1) ** (fails)) * ((fails + 1) // 2)
+            sign = (-1) ** (fails+1)
+            exponent = -((fails + 1) // 2)
+            stepMultiplier = sign * pow(2.0, exponent)
             print(f"Iteration {i+1}: no improvement for {fails} iterations, adjusting stepMultiplier to {stepMultiplier}")
         else:
             print(f"Iteration {i+1}: improving")
@@ -348,21 +363,32 @@ def doRealIterations(C0, dir, newDir, hits):
         initial_dir = ray2.D().copy() # initial direction for this iteration
         prevPlane = None
         foundBetter = False
+        drawIteration = (i + 1) == iterations and draw_last_iteration
 
         # trace similar number of bounces as original ray
-        for _ in range(len(hits) + 2):
+        for iHit in range(len(hits) + 2):
             phit = ray2.calcHit(PPlane, forceIntersect=True)
             hit = closestIntersect(ray2, prevPlane)
 
+            #if drawIteration:
+            #    p1 = ray2.P()
+            #    p2 = hit.P() if hit is not None else phit.P()
+            #    ax.plot([p1[0], p2[0]], [p1[1], p2[1]], 'red', lw=2.0, label=LABEL_RAY2_ITERATION if iHit == 0 else None)
+
             # last hit or T>0 and front face hit
-            testPPlane = hit is None or (phit.T() > 0 and np.dot(N, ray2.D()) < 0)
-            #testPPlane = hit is None or (phit.T() > 0 and np.dot(LastPlaneN, ray2.D()) < 0)
+            #testPPlane = (hit is None) or (phit.T() > 0)
+            testPPlane = (hit is None) or (phit.T() > 0 and np.dot(N, ray2.D()) < 0)
+            #testPPlane = (hit is None) or (phit.T() > 0 and np.dot(LastPlaneN, ray2.D()) < 0)
             
             if testPPlane:
                 PStar = phit.P()
                 diff = np.linalg.norm(P - PStar)**2
                 if hit is not None:
                     diff += np.linalg.norm(PStar - hit.P())**2
+                throughputPenalty = True
+                if throughputPenalty:
+                    diff *= (1 + abs(len(hits) - (iHit + 1))) # larger errors if throughput / path length differs
+                    #diff += abs(len(hits) - (iHit + 1)) # larger errors if throughput / path length differs
 
                 if diff < bestDiff:
                     bestDiff = diff
@@ -372,6 +398,7 @@ def doRealIterations(C0, dir, newDir, hits):
                     dP = ray2.transfer(phit).dP()
                     nextS = solveLinearEq(dP, P - PStar)
                     foundBetter = True
+                    print("   Found better solution with diff =", bestDiff, " nextS =", nextS)
 
             if hit is None:
                 break # finished with this iteration
@@ -385,8 +412,8 @@ def doRealIterations(C0, dir, newDir, hits):
             fails += 1
         else: fails = 0
 
-    if foundBetter:
-        print(f"Improvement on last iteration found")
+        if drawIteration:
+            trace_and_draw_actual(C0, initial_dir, hits, color='red', label=LABEL_RAY2_ITERATION)
 
     return bestDir
 
@@ -421,18 +448,35 @@ def doVirtualIterations(C0, newDir, hits):
     
     return bestDir
 
+def draw_direction(C0, dir):
+    ax.arrow(C0[0], C0[1], dir[0] * 1.5, dir[1] * 1.5, head_width=0.2, color='orange', length_includes_head=True, label=LABEL_RAY2_PRED)
 
-def draw_prediction(C0, dir, hits):
+def draw_prediction(C0, dir, hits, color='orange', label=LABEL_RAY2_PRED):
+    if(len(hits) == 0):
+        draw_direction(C0, dir)
+        return
+
     ray2 = Ray(C0, dir)
     # draw predicted ray2 path
     for hit in hits:
         hit2 = ray2.calcHit(hit.Plane(), forceIntersect=True)
         if hit2 is not None:
-            ax.plot([ray2.P()[0], hit2.P()[0]], [ray2.P()[1], hit2.P()[1]], 'orange', label=LABEL_RAY2_PRED if hit == hits[0] else None) # draw in orange
+            ax.plot([ray2.P()[0], hit2.P()[0]], [ray2.P()[1], hit2.P()[1]], color, label=label if hit == hits[0] else None) # draw in orange
+            
+            prevP = ray2.P() + ray2.dP()
             ray2 = ray2.transfer(hit2)
-            ray2 = ray2.sampleNext(hit2)
+            curP = ray2.P() + ray2.dP()
+            if draw_differentials and iterations > 0:
+                ax.plot([prevP[0], curP[0]], [prevP[1], curP[1]], color=color, linestyle='--', label=LABEL_RAY2_DIFF if hit == hits[0] else None)
 
-def trace_and_draw_actual(C0, dir, hits):
+            ray2 = ray2.sampleNext(hit2)
+            if ray2 is None:
+                break # refraction not possible
+
+def trace_and_draw_actual(C0, dir, hits, color='orange', label=LABEL_RAY2_PRED):
+    if(len(hits) == 0):
+        draw_direction(C0, dir)
+        return
     ray2 = Ray(C0, dir)
     P = hits[-1].P()
     newHits = []
@@ -448,7 +492,6 @@ def trace_and_draw_actual(C0, dir, hits):
         prevPlane = hit.Plane()
         newHits.append(hit)
         ray2 = ray2.transfer(hit)
-        ray2 = ray2.sampleNext(hit)
 
         curP = ray2.P()
         diff = np.linalg.norm(P - curP)
@@ -457,7 +500,11 @@ def trace_and_draw_actual(C0, dir, hits):
             bestDiff = diff
             index = len(newHits) - 1
 
-    draw_prediction(C0, dir, newHits[:index+1])
+        ray2 = ray2.sampleNext(hit)
+        if ray2 is None:
+            break # refraction not possible
+
+    draw_prediction(C0, dir, newHits[:index+1], color, label)
 
 # ---------------------------------------------------------------
 # Matplotlib setup
@@ -476,6 +523,7 @@ ax_sliders = [
     fig.add_axes([0.75, 0.70, 0.2, 0.03]),  # C0.y
     fig.add_axes([0.75, 0.65, 0.2, 0.03]),  # Ray.tangent_scale
     fig.add_axes([0.75, 0.60, 0.2, 0.03]),  # max bounces
+    fig.add_axes([0.75, 0.55, 0.2, 0.03]),  # draw last iteration
     fig.add_axes([0.75, 0.50, 0.2, 0.03]),  # draw differentials
     fig.add_axes([0.75, 0.45, 0.2, 0.03]),  # draw guess
     fig.add_axes([0.75, 0.40, 0.2, 0.03]),  # iterations
@@ -494,32 +542,35 @@ slider_C0y = Slider(ax_sliders[5], "C0.y", -10.0, 10.0, valinit=C0[1])
 slider_tangent_scale = Slider(ax_sliders[6], "Ray.tangent_scale", 0.001, 0.5, valinit=Ray.tangent_scale)
 # integer slider for max bounces
 slider_max_bounces = Slider(ax_sliders[7], "Max Bounces", 1, 10, valinit=max_bounces, valstep=1)
+# checkbox for draw last iteration
+checkbox_draw_last_iteration = CheckButtons(ax_sliders[8], ["Draw Last Iteration"], [draw_last_iteration])
 # checkbox for draw differentials and guess
-checkbox_draw_differentials = CheckButtons(ax_sliders[8], ["Draw Differentials"], [draw_differentials])
-checkbox_draw_guess = CheckButtons(ax_sliders[9], ["Draw Guess"], [draw_guess])
+checkbox_draw_differentials = CheckButtons(ax_sliders[9], ["Draw Differentials"], [draw_differentials])
+checkbox_draw_guess = CheckButtons(ax_sliders[10], ["Draw Guess"], [draw_guess])
 # integer slider for iterations
-slider_iterations = Slider(ax_sliders[10], "Iterations ", 1, 20, valinit=iterations, valstep=1)
+slider_iterations = Slider(ax_sliders[11], "Iterations ", 0, 20, valinit=iterations, valstep=1)
 # radio buttons for iteration strategy
-radio_iteration_strategy = RadioButtons(ax_sliders[11], iteration_strategies, active=iteration_strategy)
+radio_iteration_strategy = RadioButtons(ax_sliders[12], iteration_strategies, active=iteration_strategy)
 # radio buttons for predict strategy
-radio_predict_strategy = RadioButtons(ax_sliders[12], predict_strategies, active=predict_strategy)
+radio_predict_strategy = RadioButtons(ax_sliders[13], predict_strategies, active=predict_strategy)
 # checkbox for use speed
-checkbox_use_speed = CheckButtons(ax_sliders[13], ["Use Speed"], [useSpeed])
+checkbox_use_speed = CheckButtons(ax_sliders[14], ["Use Speed"], [useSpeed])
 # checkbox for use shear
-checkbox_use_shear = CheckButtons(ax_sliders[14], ["Use Shear"], [useShear])
+checkbox_use_shear = CheckButtons(ax_sliders[15], ["Use Shear"], [useShear])
 
 # ---------------------------------------------------------------
 # Slider callbacks
 # ---------------------------------------------------------------
 
 def update(val):
-    global C0, C1, C1_angle, max_bounces, draw_differentials, draw_guess, draw_normals, iterations, iteration_strategy, predict_strategy, useSpeed, useShear
+    global C0, C1, C1_angle, max_bounces, draw_differentials, draw_guess, draw_normals, iterations, iteration_strategy, predict_strategy, useSpeed, useShear, draw_last_iteration
     C1[0] = slider_C1x.val
     C1[1] = slider_C1y.val
     C1_angle = slider_C1a.val
     C0[0] = slider_C0x.val
     C0[1] = slider_C0y.val
     max_bounces = int(slider_max_bounces.val)
+    draw_last_iteration = checkbox_draw_last_iteration.get_status()[0]
     draw_differentials = checkbox_draw_differentials.get_status()[0]
     draw_guess = checkbox_draw_guess.get_status()[0]
     draw_normals = checkbox_draw_normals.get_status()[0]
@@ -534,7 +585,7 @@ def update(val):
 for s in [slider_C1x, slider_C1y, slider_C1a, slider_C0x, slider_C0y, slider_max_bounces, slider_tangent_scale, slider_iterations]:
     s.on_changed(update)
 
-for c in [checkbox_draw_differentials, checkbox_draw_guess, checkbox_draw_normals, radio_iteration_strategy, radio_predict_strategy, checkbox_use_speed, checkbox_use_shear]:
+for c in [checkbox_draw_differentials, checkbox_draw_guess, checkbox_draw_normals, radio_iteration_strategy, radio_predict_strategy, checkbox_use_speed, checkbox_use_shear, checkbox_draw_last_iteration]:
     c.on_clicked(update)
 
 # Initial draw
